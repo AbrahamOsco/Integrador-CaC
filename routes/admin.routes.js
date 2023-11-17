760486ec4e5f3d99b84eb74de9ff79cbcb6df626
Nicolas <138610419+naikath@users.noreply.github.com>
2023-11-17 12:24:37 -0300

    feat(routes): Add middlewares to log error message
    Now logging a more specific 404 error message in the router
    instead of using the one in the index

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,3 +1,4 @@
+// @ts-check
 const express = require('express')
 const { adminController } = require('../controllers/admin.controller.js')
 
@@ -10,6 +11,10 @@ const adminRouter = express.Router()
 adminRouter.get('/', adminController.renderAdminPage)
 
 adminRouter.delete('/delete/:id', adminController.deleteProduct)
+
+adminRouter.use((_req, res) => {
+	res.status(404).send('Called /admin with wrong HTTP method or path')
+})
 
 module.exports = {
 	adminRouter,
--- routes/shop.routes.js
+++ routes/shop.routes.js
@@ -1,3 +1,4 @@
+// @ts-check
 const express = require('express')
 const { shopController } = require('../controllers/shop.controller.js')
 
@@ -12,6 +13,10 @@ shopRouter.get('/', shopController.renderShopPage)
 shopRouter.get('/cart', shopController.renderCartPage)
 
 shopRouter.post('/cart', shopController.buyProduct)
+
+shopRouter.use((_req, res) => {
+	res.status(404).send('Called /shop with wrong HTTP method or path')
+})
 
 module.exports = {
 	shopRouter,
