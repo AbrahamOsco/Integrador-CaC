60c609e2927858bbb5d67ec51b2fe99083c0bf90
Nicolas <138610419+naikath@users.noreply.github.com>
2023-11-18 00:11:07 -0300

    feat(utils): Add function to parse data
    The float numbers received from the database
    when passed to JS have too many decimals
    This function leaves just 2 decimals

--- utils/mysql.js
+++ utils/mysql.js
@@ -31,6 +31,28 @@ function parseQuery(query) {
 	return sqlFilter
 }
 
+function formatFloat(floatNumber) {
+	return Number.parseFloat(floatNumber.toFixed(2))
+}
+
+/**
+ * Decimals for some reason are too long when retrieved from the database
+ * This function fixes that, leaving only 2 decimals
+ */
+function parseReceived(products) {
+	if (typeof products !== 'object') return
+	if (!Array.isArray(products)) {
+		products = [products]
+	}
+
+	products.forEach(product => {
+		product.product_price = formatFloat(product.product_price ?? 0)
+		product.discount = formatFloat(product.discount ?? 0)
+	})
+	return products
+}
+
 module.exports = {
 	parseQuery,
+	parseReceived,
 }
